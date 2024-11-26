9db88b54eaa3d401f1423184e4c75109729c8a69
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-11-25 22:11:05 -0300

    feat: create LinkComponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
-import { useEffect } from "react";
 export default function Home() {
-  if (true) {
-    useEffect(() => {}, []);
-  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -15,6 +11,7 @@ export default function Home() {
           height={38}
           priority
         />
+
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
