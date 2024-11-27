a8fa537c8ad3f94a4b9126f2bce98389fe940c13
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-11-27 14:40:25 -0300

    feat: create what we do and Attention from the press sections

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
 import { BitcoinIcon } from "@/components/icons/BitcoinIcon";
+import fs from "fs";
+import Image from "next/image";
+import path from "path";
 import Header from "./components/Header";
 
 export default function Home() {
+  const posters = fs.readdirSync(path.join(process.cwd(), "public/posters"));
   return (
     <>
       <Header />
@@ -137,6 +141,106 @@ export default function Home() {
               what we can achieve together.
             </p>{" "}
           </div>
+        </div>
+      </section>
+
+      <section className="mt-12">
+        <div className="layout-center">
+          <h2 className="text-heading">What we do</h2>
+          <div className="space-y-5">
+            <p className="text-lg text-black">
+              <strong className="text-primary-pink">We meet.</strong> Hosting
+              nightly sessions on Discord where we come together to code and
+              learn. The community is open to activity proposals and lessons,
+              and members can count on our engagement and peer review. Our
+              activities are outlined in the #schedule channel.
+            </p>
+            <p className="text-lg text-black">
+              <strong className="text-primary-pink">We build community.</strong>{" "}
+              We support universities in bootstrapping their Bitcoin Clubs
+              through the Bitcoin Students Network. Check out the hall of clubs
+              kickstarted and nurtured by Scalar School.
+            </p>
+          </div>
+          <a
+            className="block font-bold text-center text-2xl text-primary-pink my-6"
+            href="https://x.com/fatecbitcoin"
+            target="_blank"
+          >
+            Fatec Bitcoin Club
+          </a>
+          <a
+            className="block font-bold text-center text-2xl text-primary-pink mb-6"
+            href="https://x.com/ubtcx"
+            target="_blank"
+          >
+            UFSCar Bitcoin Club
+          </a>
+          <p className="text-lg text-black">
+            We are also proud organizers of the classic Bitdevs events in the
+            countryside of São Paulo state, covering Ribeirão Preto-SP and São
+            Carlos-SP, collectively known as Bitdevs Interior:
+          </p>
+          <a
+            className="block font-bold text-center text-2xl text-primary-pink my-6"
+            href="https://github.com/bitdevsinterior"
+            target="_blank"
+          >
+            https://github.com/bitdevsinterior
+          </a>
+          <a
+            className="block font-bold text-center text-2xl text-primary-pink my-6"
+            href="https://github.com/bitdevsinterior"
+            target="_blank"
+          >
+            https://x.com/bitdevsinterior
+          </a>
+          <p className="text-lg text-black max-w-[820px]">
+            Check out the artwork created by the students for the genesis
+            meeting of the UFSCar Bitcoin Club:
+          </p>
+          <div className="flex mt-7">
+            {posters.map((poster) => (
+              <figure key={poster} className="w-1/5">
+                <Image
+                  alt="Poster ufscar bitcoin club"
+                  src={`/posters/${poster}`}
+                  // O Next.js pegará width/height automaticamente da imagem
+                  width={0}
+                  height={0}
+                  sizes="(max-width: 768px) 100vw, 50vw"
+                  className="w-full h-auto" // Para responsividade
+                />
+              </figure>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-14">
+        <div className="layout-center">
+          <h2 className="text-heading">Attention from the press:</h2>
+          <p className="text-lg text-black">
+            We were honored to receive coverage from BeInCrypto when we launched
+            our first Bitcoin course cohort for women.
+          </p>
+          <a
+            className="block font-bold text-center text-2xl text-primary-pink my-6"
+            href="https://br.beincrypto.com/mulheres-acesso-curso-gartuito-bitcoin-brasil/"
+            target="_blank"
+          >
+            School offers a free Bitcoin course for women
+          </a>
+          <p className="text-lg text-black">
+            See this iconic post from the first cohort of Scalar School.
+          </p>
+          <a
+            className="block font-bold text-center text-2xl text-primary-pink my-6"
+            href="https://x.com/scalarschool/status/1851794719962964150"
+            target="_blank"
+          >
+            https://x.com/scalarschool/status/1851794719962964150
+          </a>
         </div>
       </section>
     </>
