fbcfca4c7ee91fc8849f95620a04206e2f0444f1
Felipe Moura <felipenascimento.moura41@gmail.com>
2024-12-08 19:09:56 -0300

    feat: create homeClient to ssg

--- src/app/home/useHome.tsx
+++ src/app/home/useHome.tsx
@@ -1,20 +1,8 @@
-import { IPeopleData } from "@/entities/people";
-import fs from "fs";
-import path from "path";
-import peopleData from "../../../people-data.json";
+"use client";
+import { getHomeData } from "@/lib/getHomeData";
+import { useState } from "react";
 
-export default function useHome() {
-  const posters = fs.readdirSync(path.join(process.cwd(), "public/posters"));
-  const typedPeopleData = peopleData as IPeopleData;
-
-  const ourPeople = fs
-    .readdirSync(path.join(process.cwd(), "public/our-people"))
-    .map((person) => ({
-      image: person,
-      name: typedPeopleData[person]?.name || "Nome não definido",
-      role: typedPeopleData[person]?.role || "Cargo não definido",
-      order: typedPeopleData[person]?.order || 999,
-    }))
-    .sort((a, b) => a.order - b.order);
-  return { posters, ourPeople };
+export function useHome(initialData: ReturnType<typeof getHomeData>) {
+  const [data] = useState(initialData);
+  return data;
 }
---
+++ src/lib/getHomeData.ts
@@ -0,0 +1,21 @@
+import { IPeopleData } from "@/entities/people";
+import fs from "fs";
+import path from "path";
+import peopleData from "../../people-data.json";
+
+export function getHomeData() {
+  const posters = fs.readdirSync(path.join(process.cwd(), "public/posters"));
+  const typedPeopleData = peopleData as IPeopleData;
+
+  const ourPeople = fs
+    .readdirSync(path.join(process.cwd(), "public/our-people"))
+    .map((person) => ({
+      image: person,
+      name: typedPeopleData[person]?.name || "Nome não definido",
+      role: typedPeopleData[person]?.role || "Cargo não definido",
+      order: typedPeopleData[person]?.order || 999,
+    }))
+    .sort((a, b) => a.order - b.order);
+
+  return { posters, ourPeople };
+}
